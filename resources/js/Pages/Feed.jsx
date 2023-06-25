e19087a4629ed325f39711c51cafb3e7c6c4522b
Marcin Maciejewski <on3n3o@gmail.com>
2023-06-25 22:17:40 +0200

    [WIP] working on /feed design

--- resources/js/Pages/Feed.jsx
+++ resources/js/Pages/Feed.jsx
@@ -6,14 +6,71 @@ export default function Feed() {
         <>
             <Head title="Explore" />
             <div class="min-h-screen flex sm:justify-center items-center pt-6 sm:pt-0 bg-black text-white">
+                {/* Here starts left sidebar */}
                 <div class="min-h-screen  border-r border-zinc-700">
                     Sidebar left
                 </div>
-                <div class="min-h-screen w-3/12">Content</div>
+
+                {/* Here starts content*/}
+                <div class="min-h-screen w-3/12">
+                    <div class="h-16 px-4 flex items-center">
+                        <p class="flex-grow font-bold text-xl">Explore</p>
+                        <p>
+                            <i class="fa fa-gear" />
+                        </p>
+                    </div>
+                    <div class="snap-y">
+                        {/* Single Tweet */}
+                        <div class="">
+                            <div class="m-4">
+                                <video
+                                    class="rounded-2xl"
+                                    controls
+                                    autoplay
+                                    loop
+                                >
+                                    <source
+                                        src="/video/test.mp4"
+                                        type="video/mp4"
+                                    />
+                                </video>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Here starts right sidebar */}
                 <div class="min-h-screen border-l border-zinc-700">
-                    Sidebar right
+                    <div class="border border-zinc-700 p-4 m-4 rounded-2xl w-full">
+                        <p class="text-xl font-bold">New to Twatter?</p>
+                        <p class="text-sm text-zinc-500 py-2">
+                            Sign up to get your own personalized timeline!
+                        </p>
+                        <div class="flex flex-col gap-2">
+                            <button class="flex items-center justify-center bg-white text-black  py-2 rounded-full border px-20 w-full">
+                                <img width="18" src="/img/g-logo.png" />
+                                <span class="px-2">Sign up with Google</span>
+                            </button>
+                            <button class="flex items-center justify-center bg-white text-black hover:bg-zinc-200 font-bold py-2 rounded-full border px-20 w-full">
+                                <img width="18" src="/img/g-logo.png" />
+                                <span class="px-2">Sign up with Apple</span>
+                            </button>
+                            <button class="bg-white text-black hover:bg-zinc-200 font-bold py-2 rounded-full border px-20 w-full">
+                                Create account
+                            </button>
+                        </div>
+                        <span class="text-sm text-zinc-500">
+                            By signing up, you agree to the{" "}
+                            <span class="text-blue-400">Terms of Service</span>{" "}
+                            and{" "}
+                            <span class="text-blue-400">Privacy Policy</span>,
+                            including{" "}
+                            <span class="text-blue-400">Cookie Use</span>.
+                        </span>
+                    </div>
                 </div>
             </div>
+
             <div class="w-full bg-blue-400 text-white absolute bottom-0 left-0 flex flex-col justify-center align-middle">
                 <div class="w-full h-16 flex justify-center align-middle text-center">
                     <div class="w-4/12 flex collapse sm:visible">
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,9 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=roboto:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/regular.min.css" integrity="sha512-WidMaWaNmZqjk3gDE6KBFCoDpBz9stTsTZZTeocfq/eDNkLfpakEd7qR0bPejvy/x0iT0dvzIq4IirnBtVer5A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        
         <!-- Scripts -->
         @routes
         @viteReactRefresh
