aafc63615d81a5e2c59d6bf8eda7eab1f163f13c
Marcin Maciejewski <on3n3o@gmail.com>
2023-06-29 22:21:16 +0200

    Add laravel socialite

--- config/services.php
+++ config/services.php
@@ -31,4 +31,14 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'oauth' => [
+        'enabled' => env('OAUTH_ENABLED'),
+        'services' => explode(',', env('OAUTH_SERVICES')),
+    ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_CLIENT_REDIRECT'),
+    ],
 ];
